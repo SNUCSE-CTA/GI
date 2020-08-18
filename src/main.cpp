6ef47801f14be41dd39c48db8f398339c04a9cad
yhnam <yhnam@theory.snu.ac.kr>
2020-08-18 16:27:21 +0900

    Measure the execution time and the number of recursive calls

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include "graph.h"
 #include "algorithm.h"
+#include "timer.h"
 
 using namespace std;
+
+double totalTime = 0;
+double searchTime = 0;
+long long numRecur = 0;
 
 int main(int argc, char* argv[])
 {
@@ -11,6 +17,8 @@ int main(int argc, char* argv[])
 		cout << "Usage: ./program graph1 graph2" << endl;
 		return -1;
 	}
+
+	Timer t;
 
 	string file1(argv[1]);
 	string file2(argv[2]);
@@ -21,13 +29,17 @@ int main(int argc, char* argv[])
 
 	//2. GI algorithm
 	Algorithm alg;
+	t.start();
 	bool isomorphic = alg.run(g1, g2);
+	totalTime = t.end();
 
 	//3. print results
 	if( isomorphic )
 		cout << "true: " << file1 << " and " << file2 << " are isomorphic" << endl;
 	else
 		cout << "false: " << file1 << " and " << file2 << " are not isomorphic" << endl;
+
+	cout << fixed << setprecision(6) << totalTime - searchTime << ' ' << searchTime << ' ' << totalTime << ' ' << numRecur << endl;
 
 	//4. deallocate input graphs from memory
 	delete g1;
