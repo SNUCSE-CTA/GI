5364f7bcda88c55e097b198cdb034a272bf803db
yhnam <yhnam@theory.snu.ac.kr>
2020-08-21 13:37:06 +0900

    add comments (file header, function header) to each file

--- include/algorithm.h
+++ include/algorithm.h
@@ -1,3 +1,13 @@
+//***************************************************************************
+//// This file is part of the graph isomorphism algorithm.
+//// Copyright by Geonmo Gu, Yehyun Nam, and Kunsoo Park
+//// 
+//// Name: algorithm.h
+//// Author: Geonmo Gu, Yehyun Nam
+//// Version
+////     August 20, 2020: the first stable version. (version 1.0)
+////***************************************************************************
+
 #ifndef __ALGORITHM_H__
 #define __ALGORITHM_H__
 
@@ -13,10 +23,17 @@ class Algorithm
 public:
 	Algorithm();
 	~Algorithm();
+
+	//parameters: [aG1], [aG2]
+	////VERIFY whether aG1 and aG2 are isomorphic
+	////RETURN true if aG1 and aG2 are isomorphic, false otherwise
 	bool run(Graph*, Graph*);
 
 private:
-	bool checkSimpleInvariants(Graph*, Graph*);  // check degree sequence, label
+	//parameters: [aG1], [aG2]
+	////VERIFY whether aG1 and aG2 has same number of vertices for each degree and label
+	////RETURN true if the condition is satisfied, false otherwise
+	bool checkSimpleInvariants(Graph*, Graph*);
 };
 
 #endif
