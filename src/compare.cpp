2904aa9f8290dae71b224ab31c2846794d20c164
yhnam <yhnam@theory.snu.ac.kr>
2020-10-15 17:12:46 +0900

    Fix bug: issue #19

--- src/compare.cpp
+++ src/compare.cpp
@@ -10,9 +10,7 @@ extern "C" {
 
 int compare(const char *fname1, const char *fname2, bool *isomorphic, double *processedTime)
 {
-    double totalTime = 0;
-    double searchTime = 0;
-    int32_t numRecur = 0;
+	double totalTime = 0;
 
 	Timer t;
 
@@ -23,13 +21,11 @@ int compare(const char *fname1, const char *fname2, bool *isomorphic, double *pr
 	Graph* g1 = new Graph(file1);
 	Graph* g2 = new Graph(file2);
 
-//    std::shared_ptr<Graph> upg1(g1);
-//    std::shared_ptr<Graph> upg2(g2);
+	std::shared_ptr<Graph> upg1(g1);
+	std::shared_ptr<Graph> upg2(g2);
 
 	//check if input files exist.
 	if(g1->fail() || g2->fail() ) {
-		delete g1;
-		delete g2;
 		return 0;
 	}
 
@@ -39,14 +35,11 @@ int compare(const char *fname1, const char *fname2, bool *isomorphic, double *pr
 	*isomorphic = alg.run(g1, g2);
 	totalTime = t.end();
 
-    if (processedTime != nullptr) {
-       *processedTime = totalTime; 
-    }
+	if (processedTime != nullptr) {
+		*processedTime = totalTime;
+	}
 
-	delete g1;
-	delete g2;
-
-    return 1;
+	return 1;
 }
 
 }
